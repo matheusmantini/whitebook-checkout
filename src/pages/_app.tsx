4a24a5d643cfbfa1bfcd5d19461ebc51b457cc3d
Matheus Mantini <engmatheusmantini@gmail.com>
2025-03-24 20:30:53 -0300

    style: adds DM Sans as app default font-family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { DM_Sans } from "next/font/google";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-dm-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={dmSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
