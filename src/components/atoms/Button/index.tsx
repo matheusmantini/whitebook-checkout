60da1582fd993a82eb649bc69ac6a0851e718cee
Matheus Mantini <engmatheusmantini@gmail.com>
2025-03-25 14:54:04 -0300

    feat(button component): add button/link component

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./button.module.scss";
+import { BUTTON_VARIANTS } from "./variants";
+import { ButtonProps } from "../../../types/buttonProps";
+
+const Button = ({
+  children,
+  variant = BUTTON_VARIANTS.Primary,
+  href,
+  disabled,
+  ariaLabel,
+  rel,
+  target,
+  fontSize,
+  fontWeight,
+  ...otherProps
+}: ButtonProps) => {
+  const className = `${styles.Button} ${styles[variant]} ${
+    disabled ? styles.Disabled : ""
+  }`;
+
+  const customStyles = {
+    fontSize: fontSize
+      ? typeof fontSize === "number"
+        ? `${fontSize}px`
+        : fontSize
+      : undefined,
+    fontWeight: fontWeight?.toString(),
+  };
+
+  if (href) {
+    return (
+      <Link
+        href={href}
+        className={className}
+        aria-disabled={disabled}
+        aria-label={ariaLabel}
+        rel={rel}
+        target={target}
+        tabIndex={disabled ? -1 : 0}
+        style={customStyles}
+        {...(otherProps as React.AnchorHTMLAttributes<HTMLAnchorElement>)}
+      >
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      className={className}
+      disabled={disabled}
+      aria-label={ariaLabel}
+      style={customStyles}
+      {...(otherProps as React.ButtonHTMLAttributes<HTMLButtonElement>)}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
