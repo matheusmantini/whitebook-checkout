e61b97998925e6f76d046adcd077d0084e079bfe
Matheus Mantini <engmatheusmantini@gmail.com>
2025-03-26 17:26:47 -0300

    refactor(tag component): adds a conditional rendering based on a new props

--- src/components/atoms/Tag/index.tsx
+++ src/components/atoms/Tag/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import styles from "./Tag.module.scss";
 import { TagProps } from "@/types/tagProps";
 
-const Tag: React.FC<TagProps> = ({ text }) => {
+const Tag: React.FC<TagProps> = ({ text, isDiscount = false }) => {
   return (
-    <div className={styles.Container}>
-      <p className={styles.TextContent}>{text}</p>
+    <div className={`${styles.Container} ${isDiscount ? styles.Discount : ""}`}>
+      <p
+        className={`${styles.TextContent} ${isDiscount ? styles.DiscountText : ""}`}
+      >
+        {text}
+      </p>
     </div>
   );
 };
