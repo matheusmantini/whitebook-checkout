6e95635c6a70a60d58f7c03c809775c783d89a72
Matheus Mantini <engmatheusmantini@gmail.com>
2025-03-27 12:01:31 -0300

    chore(paymentForm): fixes cardNumber validation to check format and requirement

--- src/components/paymentForm/paymentValidationSchema.ts
+++ src/components/paymentForm/paymentValidationSchema.ts
@@ -1,18 +1,33 @@
 import { z } from "zod";
 
 export const paymentSchema = z.object({
-  cardNumber: z
-    .string()
-    .length(19, { message: "Número do cartão inválido." })
-    .regex(/^\d{4} \d{4} \d{4} \d{4}$/, {
-      message: "Formato do número do cartão inválido.",
-    }),
+  cardNumber: z.string().min(17, { message: "Número do cartão inválido." }),
   cardExpirationDate: z
     .string()
     .length(5, { message: "Data de expiração inválida." })
     .regex(/^\d{2}\/\d{2}$/, {
       message: "Formato da data de expiração inválido.",
-    }),
+    })
+    .refine(
+      (date) => {
+        const [month, year] = date.split("/").map(Number);
+        if (month < 1 || month > 12) return false;
+
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear() % 100;
+        const currentMonth = currentDate.getMonth() + 1;
+
+        if (
+          year < currentYear ||
+          (year === currentYear && month < currentMonth)
+        ) {
+          return false;
+        }
+
+        return true;
+      },
+      { message: "O cartão expirou ou a data é inválida." },
+    ),
   cardCVV: z
     .string()
     .length(3, { message: "CVV inválido." })
@@ -30,5 +45,12 @@ export const paymentSchema = z.object({
   discountCoupon: z.string().optional(),
   installments: z
     .string()
-    .min(1, { message: "Selecione o número de parcelas" }),
+    .optional()
+    .refine(
+      (value) => {
+        if (value === undefined || value === "") return true;
+        return /^\d+$/.test(value);
+      },
+      { message: "Selecione o número de parcelas" },
+    ),
 });
