a056565a06451bd3cd90054bdc99b8d6a2d877e3
Matheus Mantini <engmatheusmantini@gmail.com>
2025-03-28 08:38:58 -0300

    fix(input): fixes input type

--- src/components/molecules/Input/types.ts
+++ src/components/molecules/Input/types.ts
@@ -1,15 +1,13 @@
-// types.ts
 import { FieldError } from "react-hook-form";
-import { IMask } from "imask";
 import { MASKS } from "./masks";
+import React from "react";
 
 export type MaskType = keyof typeof MASKS | string;
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement>,
-    IMask.MaskOptions {
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
   label?: string;
+  type?: string;
+  register?: any;
+  maskKey?: keyof typeof MASKS;
   error?: FieldError;
-  maskType?: MaskType;
-  onAccept?: (value: string) => void;
 }
